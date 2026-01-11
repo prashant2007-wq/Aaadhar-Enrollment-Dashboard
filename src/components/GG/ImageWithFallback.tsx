2a796afcc83939619518ff096d46d3c3cf7c9dd8
PRASHANT S BISHT <prashantjee2025@gmail.com>
2026-01-11 18:49:11 +0530

    Update ImageWithFallback.tsx

--- src/components/GG/ImageWithFallback.tsx
+++ src/components/GG/ImageWithFallback.tsx
@@ -19,5 +19,5 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
   ) : (
     <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
   ) 
-}
+} 
 
